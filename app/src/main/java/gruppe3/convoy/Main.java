d8ed55d5971368a146f708f4932714ecd24b7d56
ministeren <ministeren>
2015-11-16 10:15:19 +0100

    Første fragment

--- app/src/main/java/gruppe3/convoy/Main.java
+++ app/src/main/java/gruppe3/convoy/Main.java
@@ -1,13 +1,23 @@
 package gruppe3.convoy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
 
 public class Main extends AppCompatActivity {
 
+    FrameLayout frame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frame = (FrameLayout) findViewById(R.id.knapFrame);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.knapFrame, new MainButtonsFragment())
+                .commit();
     }
+
 }
