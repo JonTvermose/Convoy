f0053b87388f9c602d0241ffaa68137006204207
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2015-11-25 15:56:12 +0100

    Tilføjelse af dialog-popup når man klikker på en marker. Mangler stadig "lidt" rent designmæssigt... ;-) Tilføjet clickListeners til popup m.m.

--- app/src/main/java/gruppe3/convoy/GMapsAktivitet.java
+++ app/src/main/java/gruppe3/convoy/GMapsAktivitet.java
@@ -1,22 +1,22 @@
 package gruppe3.convoy;
 
-import android.content.Context;
-import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Activity;
-import android.provider.Settings;
-import android.util.Log;
+import android.app.Dialog;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -30,6 +30,7 @@ public class GMapsAktivitet extends Activity implements OnMapReadyCallback {
     private GoogleMap googleMap;
     private Location lastKnownLocation;
     private MyLocation locationListener;
+    private BackendSimulator backend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +64,7 @@ public class GMapsAktivitet extends Activity implements OnMapReadyCallback {
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
 
-        if(locationListener.getLocation() != null){
+        if (locationListener.getLocation() != null) {
             LatLng cPos = new LatLng(locationListener.getLocation().getLatitude(), locationListener.getLocation().getLongitude());
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cPos, 12));
         } else {
@@ -74,14 +75,97 @@ public class GMapsAktivitet extends Activity implements OnMapReadyCallback {
 
         Marker mark1 = googleMap.addMarker(new MarkerOptions().position(new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude())).title("Sidst kendte sted"));
 
-        BackendSimulator backend = new BackendSimulator();
-        for (Spot spot : backend.getMarkers()){
+        backend = new BackendSimulator();
+        for (Spot spot : backend.getMarkers()) {
             Marker mark = googleMap.addMarker(new MarkerOptions().position(spot.getPos()).title(spot.getDesc()));
             mark.setDraggable(false);
         }
 
-        locationListener.setMap(googleMap);
 
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                final Dialog dialog = new Dialog(GMapsAktivitet.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+                //AlertDialog.Builder dialog = new AlertDialog.Builder(GMapsAktivitet.this);
+                dialog.setContentView(R.layout.fragment_spot);
+                Spot spot = getSpot(marker.getTitle());
+                TextView title = (TextView) dialog.findViewById(R.id.title_TextView);
+                title.setText(spot.getDesc());
+
+                ImageView adblue = (ImageView) dialog.findViewById(R.id.adblue_imageView);
+                ImageView bed = (ImageView) dialog.findViewById(R.id.bed_imageView);
+                ImageView bath = (ImageView) dialog.findViewById(R.id.bath_imageView);
+                ImageView food = (ImageView) dialog.findViewById(R.id.food_imageView);
+                ImageView fuel = (ImageView) dialog.findViewById(R.id.fuel_imageView);
+                ImageView wc = (ImageView) dialog.findViewById(R.id.wc_imageView);
+
+                if (spot.isAdblue()) {
+                    adblue.setImageResource(R.drawable.adblue_t_check);
+                } else {
+                    adblue.setImageResource(R.drawable.adblue_t);
+                }
+                if (spot.isBath()) {
+                    bath.setImageResource(R.drawable.bath_t_check);
+                } else {
+                    bath.setImageResource(R.drawable.bath_t);
+                }
+                if (spot.isBed()) {
+                    bed.setImageResource(R.drawable.bed_t_check);
+                } else {
+                    bed.setImageResource(R.drawable.bed_t);
+                }
+                if (spot.isFood()) {
+                    food.setImageResource(R.drawable.food_t_check);
+                } else {
+                    food.setImageResource(R.drawable.food_t);
+                }
+                if (spot.isFuel()) {
+                    fuel.setImageResource(R.drawable.fuel_t_check);
+                } else {
+                    fuel.setImageResource(R.drawable.fuel_t);
+                }
+                if (spot.isWc()) {
+                    wc.setImageResource(R.drawable.wc_t_check);
+                } else {
+                    wc.setImageResource(R.drawable.wc_t);
+                }
+
+                dialog.show();
+
+                Button route = (Button) dialog.findViewById(R.id.findRoute_button);
+                route.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(GMapsAktivitet.this, "Finding route. Please wait...", Toast.LENGTH_LONG).show();
+                        dialog.hide();
+
+                    }
+                });
+
+                ImageView close = (ImageView) dialog.findViewById(R.id.close_imageView);
+                close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(GMapsAktivitet.this, "Closing dialog", Toast.LENGTH_SHORT).show();
+                        dialog.hide();
+                    }
+                });
+
+                return true;
+            }
+        });
+        locationListener.setMap(googleMap, this);
+    }
+
+    private Spot getSpot(String desc){
+        for (Spot spot : backend.getMarkers()){
+            if(spot.getDesc().equals(desc)){
+                return spot;
+            }
+        }
+        return new Spot("Overskrift her", false, false, false, false, false,false, false, new LatLng(55.5,16.324));
     }
 
 }
--- app/src/main/java/gruppe3/convoy/functionality/MyLocation.java
+++ app/src/main/java/gruppe3/convoy/functionality/MyLocation.java
@@ -1,9 +1,11 @@
 package gruppe3.convoy.functionality;
 
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,6 +18,7 @@ public class MyLocation implements LocationListener {
 
     private Location current;
     private GoogleMap map;
+    private Context context;
     private boolean adjustedCamera;
     private final int ZOOM = 12;
 
@@ -30,6 +33,7 @@ public class MyLocation implements LocationListener {
         if (!adjustedCamera){
             adjustedCamera = true;
             updateMapView();
+            Toast.makeText(context, "Location updated", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -52,12 +56,13 @@ public class MyLocation implements LocationListener {
         return current;
     }
 
-    public void setMap(GoogleMap googleMap) {
+    public void setMap(GoogleMap googleMap, Context context) {
         this.map = googleMap;
+        this.context = context;
     }
 
     public void updateMapView(){
         LatLng cPos = new LatLng(current.getLatitude(), current.getLongitude());
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(cPos, ZOOM));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(cPos, ZOOM), 2000, null);
     }
 }
