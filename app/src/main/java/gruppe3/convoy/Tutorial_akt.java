cf448fcab7c7c7b027d0f8207d8997e59bbcfbe5
Ebbe <Ebbeb2424@gmail.com>
2016-01-19 12:25:12 +0100

    Tilføjelse: Påbegyndt en tutorial til appen

---
+++ app/src/main/java/gruppe3/convoy/Tutorial_akt.java
@@ -0,0 +1,14 @@
+package gruppe3.convoy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tutorial_akt extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+    }
+
+}
